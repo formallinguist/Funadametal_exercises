374b4e7877e063f6f7d1f6705520118daf79084f
Ravi Kiran Chikkala <97979636+formallinguist@users.noreply.github.com>
2023-01-03 23:09:36 +0100

    Add files via upload

---
+++ addition_of_numbers.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+	int x,y,z;
+	printf("Enter the first number\n");
+	scanf("%d",&x);
+	printf("Enter the second number\n");
+	scanf("%d",&y);
+	z = x+y;
+	printf("The addition of %d and %d is %d",x,y,z);
+}
