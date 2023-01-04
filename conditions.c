02a60deaf53e6e542982c945c80491dc3039fec1
Ravi Kiran Chikkala <97979636+formallinguist@users.noreply.github.com>
2023-01-05 00:02:09 +0100

    Add files via upload

---
+++ conditions.c
@@ -0,0 +1,23 @@
+//decesion making statements.
+
+#include<stdio.h>
+
+void main()
+{
+	int x;
+	printf("Enter x\n");
+	scanf("%d",&x);
+	if (x==10)
+	{
+		printf("x is 10\n");
+    }
+    else if (x == 6)
+    {
+    	printf("x is 6\n");
+	}
+	else
+	{
+		printf("x is not 10 or 6");
+	}
+
+}
